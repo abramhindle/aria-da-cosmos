5c8e1c50babbb0db5445c76283c24df80db151fc
Abram Hindle <abram.hindle@softwareprocess.es>
2015-10-08 21:57:36 -0600

    midi

--- midi/score.pl
+++ midi/score.pl
@@ -3,6 +3,9 @@ use strict;
 my @hands = (
 	[map { 0 } (0..127)],#useless hand. Ha!
 	[map { 0 } (0..127)],
+	[map { 0 } (0..127)],
+	[map { 0 } (0..127)],
+	[map { 0 } (0..127)],
 	[map { 0 } (0..127)]
 );
 sub midi2time {
@@ -14,8 +17,11 @@ while(<>) {
 	chomp;
 	s/^\s*//;
 	s/\s*$//;
-	my ($time,$hand,$midi,$vel) = split(/\s*,\s*/,$_);
-	if ($hands[$hand][$midi] > 0) {
+	#my ($hand,$time,$type,$something,$midi,$vel) = split(/\s*,\s*/,$_);
+	my ($track, $time, $type, $channel, $midi, $vel) = split(/\s*,\s*/,$_);
+	my $hand  = $track;
+        next unless $type =~ /Note_o/;
+	if ($hands[$hand][$midi] > 0 && ($type eq "Note_off_c" || $vel == 0)) {
 		print join(",",midi2time($hands[$hand][$midi]),midi2time($time-$hands[$hand][$midi]),$hand,$midi,$/);
 		$hands[$hand][$midi] = 0;
 	} else {
